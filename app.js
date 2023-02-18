6486934ef9deca840362b93f4643e89ca22dcb76
Nicolas Agustin Marco <nicolasmarco841@gmail.com>
2023-02-18 12:51:37 -0300

    Primer commit. Todo el proyecto subido

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const path = require ("path");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/" , (req , res) => {
+    
+    const rutaCompleta = path.join(__dirname , "/views/home.html");
+    res.sendFile(rutaCompleta);
+
+});
+
+app.listen(3000, () => {
+    console.log("Servidor funcionando en el puerto 3000");
+});
